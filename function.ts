e40a1bf5f232a25a0f346fea9b7b01ea5e1d110c
Bannawat <109212993+Bannawat01@users.noreply.github.com>
2025-06-09 21:54:14 +0700

    func

---
+++ function.ts
@@ -0,0 +1,14 @@
+//name of functions
+function hello(name: string) {
+    return `Hello ,${name}`
+}
+console.log(hello('BannawatKung'))
+
+//anonymous func
+let kamuy = function (): string {
+    return 'eiei'
+}
+
+//arrow func or lamda
+let gojo = () => "nah i'd win."
+console.log(gojo())
