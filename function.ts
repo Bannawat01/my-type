54e6c28f16a8f6a378a8ee8f63eb7db9423bd5bd
Bannawat <109212993+Bannawat01@users.noreply.github.com>
2025-06-09 22:06:51 +0700

    init

--- function.ts
+++ function.ts
@@ -2,7 +2,7 @@
 function hello(name: string) {
     return `Hello ,${name}`
 }
-console.log(hello('BannawatKung'))
+// console.log(hello('BannawatKung'))
 
 //anonymous func
 let kamuy = function (): string {
@@ -11,4 +11,25 @@ let kamuy = function (): string {
 
 //arrow func or lamda
 let gojo = () => "nah i'd win."
-console.log(gojo())
+// console.log(gojo())
+
+let sum = (...nums: number[]): number => {
+    let sum = 0
+    for (let index = 0; index < nums.length; index++) {
+        sum += nums[index]
+    }
+    return sum
+}
+function avg(...nums: number[]): number {
+    return sum(...nums) / nums.length
+}
+function do_something(nums_array: number[], func: (...nums: number[]) => number): number {
+    return func(...nums_array)
+}
+
+let nums = [50, 90, 1]
+let x = do_something(nums, avg) // <- Passed as an argument
+console.log(x) // 47
+x = do_something(nums, sum) // <- Passed as an argument
+console.log(x) // 141
+
